abaa3c2776264f3185bda08032e9fdd2ca8b8bc8
LordA2117 <vip20dpa@gmail.com>
2025-04-30 21:59:13 +0530

    Added gitignore

--- app.py
+++ app.py
@@ -8,6 +8,14 @@ app.config['SECRET_KEY'] = os.urandom(12).hex()
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///example.sqlite"
 
 # Building and designing the database
+# Sorry I didn't get enough time to change the SqlAlchemy settings to Postgres :(
 
 class Base(DeclarativeBase):
     pass
+
+db = SQLAlchemy(app, model_class=Base)
+
+class User(db.Model):
+    id: Mapped[int] = mapped_column(db.Integer, primary_key=True, autoincrement=True)
+    username: Mapped[str] = mapped_column(db.String, nullable=False)
+    password: Mapped[str] = mapped_column(db.String, nullable=False)
